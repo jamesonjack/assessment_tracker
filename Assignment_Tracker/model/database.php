5ded73a24c748d1bba498dc36323e3b7c8e9997f
jamesonjack <jackjamesonwebd@gmail.com>
2022-02-16 21:01:41 +1000

    Update database.php

--- Assignment_Tracker/model/database.php
+++ Assignment_Tracker/model/database.php
@@ -1,14 +1,34 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=assignment_tracker';
-    $username = 'root';
-    //$password = 'pa55word';
+
+//$dsn = 'mysql:host=localhost;dbname=assignment_tracker';
+//$username = 'root';
+//$password = 'pa55word';
+
+//try {/
+   // $db = new PDO($dsn, $username);
+   // //$db = new PDO($dsn, $username, $password);
+// }catch (PDOException $e) {
+   // $error = "Database Error: ";
+   // $error .= $e->getMessage();
+   // include('view/error.php');
+ //   exit();
+//}
+
+
+//remote connection
+    $dsn = 'mysql:host=remotemysql.com;dbname=Q7cbSugRvM';
+    $username = 'Q7cbSugRvM';
+    $password = 'FJPrD1h2LI';
 
     try {
-        $db = new PDO($dsn, $username);
-        //$db = new PDO($dsn, $username, $password);
+       // $db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error = "Database Error: ";
         $error .= $e->getMessage();
         include('view/error.php');
         exit();
     }
+    session_start();
+
+ ?>
